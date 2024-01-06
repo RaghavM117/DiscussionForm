7ae6b2fa3e3e1901ba928b22c30f370145e7e5cc
RaghavM117 <140271275+RaghavM117@users.noreply.github.com>
2024-01-06 08:52:14 +0530

    done

---
+++ DiscussionForm/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from forum.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path('addInForum/',addInForum,name='addInForum'),
+    path('addInDiscussion/',addInDiscussion,name="addInDiscussion"),
+]
